d8cdb0972836e7b555a1a7f3442e82ee8ce09cf1
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-13 15:02:05 +0200

    imageInput

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+/*
+1.creare l oggetto immagini
+2.sentire il click a next e prev
+3.contatore immagini
+4.assegnazione classe active
+5.limitare contatore per ciclare immagini caricate
+6.
+ */
+
+
+
 const { createApp } = Vue ;
 
 createApp( {
   data() {
     return {
+      images : ["assets-slider/img/01.webp",
+                "assets-slider/img/02.webp",
+                "assets-slider/img/03.webp",
+                "assets-slider/img/04.webp",
+                "assets-slider/img/05.webp",
+                ],
+
 
     }
   },
