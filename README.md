ce045b582642b032967526846aa61f3b3efc4c71
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-13 20:08:39 +0200

    thumb active

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Descrizione:
 
-Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue. -----
 
 Bonus:
 
--- js/script.js
+++ js/script.js
@@ -14,15 +14,24 @@ const { createApp } = Vue ;
 createApp( {
   data() {
     return {
-      images : ["assets-slider/img/01.webp",
-                "assets-slider/img/02.webp",
-                "assets-slider/img/03.webp",
-                "assets-slider/img/04.webp",
-                "assets-slider/img/05.webp",
-                ],
+      images : [
+        "assets-slider/img/01.webp",
+        "assets-slider/img/02.webp",
+        "assets-slider/img/03.webp",
+        "assets-slider/img/04.webp",
+        "assets-slider/img/05.webp",],
       
-      indexCounter : 3,
-      inputCounter : 0,         
+      inputCounter : 0,
+      active:'active',
+      thumbs : {
+        thumb1 : 'act',
+        thumb2 : '',
+        thumb3 : '',
+        thumb4 : '',
+        thumb5 : '',
+    },
+
+
 
     }
   },
@@ -30,24 +39,55 @@ createApp( {
   methods: {
     next: function(){
       if (this.inputCounter > this.images.length -2 ) {
-        return this.inputCounter = 0;  
+        return this.inputCounter = 0 , this.addActive(); 
         }
         return this.inputCounter++,
-        console.log('next',this.inputCounter, this.images.length - 1)
-      
+        console.log('next',this.inputCounter, this.images.length - 1), this.addActive();       
     },
-    prev: function(){
-      if (this.inputCounter < 1 ) {
-        this.inputCounter = this.images.length;  
-        }
-      return this.inputCounter--, 
-      console.log('prev',this.inputCounter)
 
+    prev: function(){
+
+      if (this.inputCounter <= 0 ) {
+        return this.inputCounter = this.images.length -1, this.addActive();  
+        }else
+        return this.inputCounter--, 
+        console.log('prev',this.inputCounter) , this.addActive();  
     },
+
+    addActive: function() {
+      this.thumbs = {
+        thumb1 : '',
+        thumb2 : '',
+        thumb3 : '',
+        thumb4 : '',
+        thumb5 : '',
+    }
+      inputC = this.inputCounter ;
+
+      if (inputC == 0) {
+        return this.thumbs.thumb1 = 'active', console.log('return if',this.thumbs)
+      }else if (inputC == 1) {
+        return this.thumbs.thumb2 = 'active', console.log('return if',this.thumbs)
+      }else if (inputC == 2) {
+        return this.thumbs.thumb3 = 'active', console.log('return if',this.thumbs)
+      }else if (inputC == 3) {
+        return this.thumbs.thumb4 = 'active', console.log('return if',this.thumbs)
+      }else if (inputC == 4) {
+        return this.thumbs.thumb5 = 'active', console.log('return if',this.thumbs)
+      }
+      return console.log('return else',this.thumbs.thumb1)
+  }
+
+    // thumbsScroll(inCount, n){
+
+    // }
 
   },
   
   mounted(){
+    this.addActive();
+    console.log(this.thumbs.thumb1)
     console.log('mounted')
   }
+
 }).mount('#app')
